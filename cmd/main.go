1b849a2fe2a05b1061ee2c9aa9afba96d730dcf0
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-08-23 23:53:58 +0900

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/google/uuid"
+)
+
+func umlToImage(uml string) (io.Reader, error) {
+	uid := uuid.New().String()
+	umlFileName := "uml/" + uid + ".pu"
+	umlFile, err := os.Create(umlFileName)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		_ = umlFile.Close()
+	}()
+
+	if _, err := umlFile.Write([]byte(uml)); err != nil {
+		return nil, err
+	}
+
+	cmd := exec.Command("java", "-jar", "plantuml.jar", "-o", "out", umlFileName)
+	if err := cmd.Run(); err != nil {
+		return nil, err
+	}
+
+	return os.Open("uml/out/" + uid + ".png")
+}
+
+func onMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if strings.HasPrefix(m.Content, "```uml") && strings.HasSuffix(m.Content, "```") {
+		image, err := umlToImage(m.Content[6 : len(m.Content)-3])
+		if err != nil {
+			log.Println(err)
+			_, err := s.ChannelMessageSend(m.ChannelID, "Failed to convert uml")
+			if err != nil {
+				log.Println(err)
+			}
+			return
+		}
+		_, err = s.ChannelFileSend(m.ChannelID, "uml.png", image)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("invalid arguments length")
+	}
+
+	session, err := discordgo.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	session.Token = "Bot " + os.Args[1]
+	session.AddHandler(onMessage)
+	if err := session.Open(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("uml2image discord bot started")
+	<-make(chan bool)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/minami14/uml2image
+
+go 1.12
+
+require (
+	github.com/bwmarrin/discordgo v0.19.0
+	github.com/google/uuid v1.1.1
+)
